3dd655d5b2c081411d1db9796d77253636e51431
Jacob <jacobanks@gmail.com>
2015-09-29 22:37:39 -0500

    first commit

---
+++ First C++ Game/main.cpp
@@ -0,0 +1,110 @@
+
+//
+// Disclamer:
+// ----------
+//
+// This code will work only if you selected window, graphics and audio.
+//
+// Note that the "Run Script" build phase will copy the required frameworks
+// or dylibs to your application bundle so you can execute it on any OS X
+// computer.
+//
+// Your resource files (images, sounds, fonts, ...) are also copied to your
+// application bundle. To get the path to these resource, use the helper
+// method resourcePath() from ResourcePath.hpp
+//
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+// Here is a small helper for you ! Have a look.
+#include "ResourcePath.hpp"
+
+int main(int, char const**)
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // Set the Icon
+//    sf::Image icon;
+//    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
+//        return EXIT_FAILURE;
+//    }
+//    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
+    // Load a sprite to display
+    sf::Texture texture;
+    if (!texture.loadFromFile(resourcePath() + "sprite.png", sf::IntRect(30, 30, 30, 30))) {
+        return EXIT_FAILURE;
+    }
+    sf::Sprite sprite(texture);
+    
+    // Create a graphical text to display
+    sf::Font sansationFont;
+    if (!sansationFont.loadFromFile(resourcePath() + "sansation.ttf")) {
+        return EXIT_FAILURE;
+    }
+    sf::Text text("First c++ Game", sansationFont, 30);
+    text.setColor(sf::Color::White);
+
+    // Load a music to play
+//    sf::Music music;
+//    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
+//        return EXIT_FAILURE;
+//    }
+
+    // Play the music
+//    music.play();
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            // Escape pressed: exit
+//            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Q) {
+//                window.close();
+//            }
+            
+            // Left button pressed
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                // left key is pressed: move our character
+                sprite.move(-1, 0);
+                
+            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                //move sprite right if right is press
+                sprite.move(1, 0);
+                
+            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+                //move sprite down if down is pressed
+                sprite.move(0, 1);
+                
+            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+                // move sprite up if up is pressed
+                sprite.move(0, -1);
+            }
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
